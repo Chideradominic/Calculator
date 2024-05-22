c3170ad865458f892a0102b763366edc2f72d669
Chideradominic <ibeduchideradominic@gmail.com>
2024-05-22 14:45:33 +0100

    Finished

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 //get display
-const display= document.getElementById("#display");
-//append display function
+const display= document.getElementById("display");
 function appendToDisplay(input){
-
+  display.value += input;
 }
+    
 //clear display function
 function clearDisplay(){
-
+ display.value = "";
 }
-//calculate
+//calculate function
 function calculate(){
+  try{
+    display.value = eval(display.value);
+  }
+  catch(error){
+    display.value ='Syntax Error';
 
+  }
 }
