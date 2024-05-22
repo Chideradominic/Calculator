826ffdcc1b5d4ee65c5a13310407a5de8d1753e8
Chideradominic <ibeduchideradominic@gmail.com>
2024-05-22 13:34:48 +0100

    Scripting the calculator

---
+++ script.js
@@ -0,0 +1,14 @@
+//get display
+const display= document.getElementById("#display");
+//append display function
+function appendToDisplay(input){
+
+}
+//clear display function
+function clearDisplay(){
+
+}
+//calculate
+function calculate(){
+
+}
